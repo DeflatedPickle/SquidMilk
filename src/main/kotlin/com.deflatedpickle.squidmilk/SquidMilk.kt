385b5368c1427d9dda8b6885a3851e838ae22e87
DeflatedPickle <deflatedpickle@gmail.com>
2021-06-01 02:46:14 +0100

    Updated to 1.14

--- src/main/kotlin/com.deflatedpickle.squidmilk/SquidMilk.kt
+++ src/main/kotlin/com.deflatedpickle.squidmilk/SquidMilk.kt
@@ -4,51 +4,34 @@
 
 package com.deflatedpickle.squidmilk
 
-import com.deflatedpickle.squidmilk.SquidMilk.ACCEPTED_VERSIONS
-import com.deflatedpickle.squidmilk.SquidMilk.ADAPTER
-import com.deflatedpickle.squidmilk.SquidMilk.DEPENDENCIES
 import com.deflatedpickle.squidmilk.SquidMilk.MOD_ID
-import com.deflatedpickle.squidmilk.SquidMilk.NAME
-import com.deflatedpickle.squidmilk.SquidMilk.VERSION
-import net.minecraft.entity.passive.EntitySquid
-import net.minecraft.init.Items
-import net.minecraft.init.SoundEvents
+import net.minecraft.entity.passive.SquidEntity
 import net.minecraft.item.ItemStack
-import net.minecraft.util.EnumHand
+import net.minecraft.item.Items
+import net.minecraft.util.Hand
+import net.minecraft.util.SoundEvents
 import net.minecraft.util.math.AxisAlignedBB
+import net.minecraft.util.math.BlockPos
 import net.minecraftforge.event.entity.player.FillBucketEvent
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract
+import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
-@Mod(
-    modid = MOD_ID,
-    name = NAME,
-    version = VERSION,
-    acceptedMinecraftVersions = ACCEPTED_VERSIONS,
-    dependencies = DEPENDENCIES,
-    modLanguageAdapter = ADAPTER
-)
+@Mod(MOD_ID)
 @Mod.EventBusSubscriber
 object SquidMilk {
     const val MOD_ID = "squidmilk"
-    const val NAME = "SquidMilk"
-    const val VERSION = "1.12.2-2.0.0.0"
-    const val ACCEPTED_VERSIONS = "[1.12,1.12.2]"
-    const val DEPENDENCIES = "required-after:forgelin;"
-    const val ADAPTER = "net.shadowfacts.forgelin.KotlinAdapter"
 
     @SubscribeEvent
-    @JvmStatic
     // Problem: If a squid in water is milked, it would get rid of the water
     // Solution: This checks if there are any squids around the block position for the event,
     // if there are, it cancels the event to fill the bucket with water
     fun onItemRightClick(event: FillBucketEvent) {
         event.world.let { world ->
-            event.target?.blockPos?.let { blockPos ->
+            event.target?.hitVec?.let { vec3d ->
                 world.getEntitiesWithinAABBExcludingEntity(
                     null,
-                    AxisAlignedBB(blockPos)
+                    AxisAlignedBB(BlockPos(vec3d))
                 ).let { entities ->
                     if (entities.isNotEmpty()) {
                         event.isCanceled = true
@@ -59,17 +42,16 @@ object SquidMilk {
     }
 
     @SubscribeEvent
-    @JvmStatic
     // Problem: Squids can't be milked
     // Solution: This lets squids be milked
     fun onEntityInteractEvent(event: EntityInteract) {
-        if (event.hand == EnumHand.OFF_HAND) return
+        if (event.hand == Hand.OFF_HAND) return
 
         event.target?.let { entity ->
-            if (entity is EntitySquid) {
-                event.entityPlayer?.let { player ->
+            if (entity is SquidEntity) {
+                event.player?.let { player ->
                     if (player.heldItemMainhand.item == Items.BUCKET) {
-                        if (!player.world.isRemote && !player.capabilities.isCreativeMode) {
+                        if (!player.world.isRemote && !player.isCreative) {
                             player.heldItemMainhand.shrink(1)
                         }
 
